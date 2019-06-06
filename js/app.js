dbb1958eea6957368e991fb3de73ecc2630496e3
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-06 14:40:41 -0700

    Everything.

--- js/app.js
+++ js/app.js
@@ -13,100 +13,118 @@
   Answer No
 */ 
 
+var correctAnswers = 0;
+var answerValidity = false;
+
+var promptQueries = [
+  'Is Brandon a gamer boi?',
+  'Do the songbirds envy Brandon?',
+  'Does Brandon even lift...brah?',
+  'Does Brandon ingest of the hated substance of the pit...LICORICE!?',
+  'Is Brandon preoccupied by the desires of the masses?'
+];
+
+var correctAlertMessages = [
+  'Accurate.',
+  'Well done.',
+  'Indeed.',
+  'This is in alignment...',
+  'You speak truly.'
+];
+
+var incorrectAlertMessages = [
+  'Hrrmm...that is not accurate.',
+  'Are you certain that you know of the being, Brandon...?',
+  'Are you a fool?',
+  'You are not a particularly sharp entity are you?',
+  'To be so wrong...how can such a thing even draw breath? Much less exist?'
+];
+
+var nonsenseAlertMessages = [
+  'This is not the arena for frivolity...',
+  'Your path is one of folly and error.',
+  'You test the patience of powers cosmic...',
+  'I will sacrifice you to the ravening pit for your impudence.',
+  'Your soul will agony long after the stars have all grown cold.'
+];
+
+// Console log messages
+var correctConsoleLogMessages = [
+  'The entity responded to the first query in the affirmative. Correct.',
+  'The entity responded to the second query in the negative. Correct.',
+  'The entity responded to the third query in the affirmative. Correct.',
+  'The entity responded to the fourth query in the negative. Correct.',
+  'The entity responded to the fifth query in the negative. Correct.'
+];
+
+var incorrectConsoleLogMessages = [
+  'The entity responded to the first query in the negative. Incorrect.',
+  'The entity responded to the second query in the affirmative. Incorrect.',
+  'The entity responded to the third query in the negative. Incorrect.',
+  'The entity responded to the fourth query in the affirmative. Incorrect.',
+  'The entity responded to the fifth query in the affirmative. Incorrect.'
+];
+
+var nonsenseConsoleMessage = 'The entity thinks itself funny. It believes it knows humor. The Eldritch Truth will teach it of humor.';
+
+var htmlTargets = [
+  'query1Answer',
+  'query2Answer',
+  'query3Answer',
+  'query4Answer',
+  'query5Answer'
+];
+
+
 alert('Well met, entity. I will now pose you with queries about the being known as Brandon, as is my grand purview. You will respond in either an affirmative or a negative, in the form of a "yes" or a "no", "Y" and "n" may also be substituted for either of these choices. Your ultimate worth on the cosmic stage shall be weighed, measured, and decided...');
 
-// Question/Answer #1
-var gamerBoi = prompt('Is Brandon a gamer boi?').toLowerCase();
-// If they say yes.
-if (gamerBoi === 'yes' || gamerBoi === 'y') {
-  alert('Accurate.');
-  document.getElementById('query1Answer').innerHTML = gamerBoi;
-  console.log('The entity responded to the first query in the affirmative. Correct.');
-} else if (gamerBoi === 'no' || gamerBoi === 'n') {
-  // If they say no.
-  alert('Hrrmm...that is not accurate.');
-  document.getElementById('query1Answer').innerHTML = gamerBoi;
-  console.log('The entity responded to the first query in the negative. Incorrect.');
-} else {
-  // If they say anything else.
-  alert('This is not the arena for frivolity...');
-  document.getElementById('query1Answer').innerHTML = gamerBoi;
-  console.log('The entity thinks itself funny. It believes it knows humor. The Eldritch Truth will teach it of humor.');
+
+for (var i = 0; i < 5; i++) {
+  var queryReplies = prompt(promptQueries[i]).toLowerCase();
+  document.getElementById(htmlTargets[i]).innerHTML = queryReplies;
+
+  // I would need a counter here to track which question is being shown to the page
+
+  switch (queryReplies) {
+  case  = 'yes', 'y':
+    queryReplies = true;
+    break;
+  case 'no', 'n':
+    queryReplies = true;
+    break; 
+  default:
+    break;
+  }
+  
+  // Check replies to the queries
+  if (queryReplies === true) {
+
+    // If they answer correctly
+    alert(correctAlertMessages[i]);
+    console.log(correctConsoleLogMessages[i]);
+    correctAnswers++;
+  }
+  else if (queryReplies === false) {
+
+    // If they answer incorrectly.
+    alert(incorrectAlertMessages[i]);
+    console.log(incorrectConsoleLogMessages[i]);
+  }
+  else {
+
+    // If they do anything else.
+    alert(nonsenseAlertMessages[i]);
+    console.log(nonsenseConsoleMessage);
+  }
 }
 
-// Question/Answer #2
-var singerBoi = prompt('Do the songbirds envy Brandon?').toLowerCase();
-// If they say yes.
-if (singerBoi === 'yes' || singerBoi === 'y') {
-  alert('Are you certain that you know of the being, Brandon...?');
-  document.getElementById('query2Answer').innerHTML = singerBoi;
-  console.log('The entity responded to the second query in the affirmative. Incorrect.');
-} else if (singerBoi === 'no' || singerBoi === 'n') {
-  // If they say no.
-  alert('Well done.');
-  document.getElementById('query2Answer').innerHTML = singerBoi;
-  console.log('The entity responded to the second query in the negative. Correct.');
-} else {
-  // If they say anything else.
-  alert('Your path is one of folly and error.');
-  document.getElementById('query2Answer').innerHTML = singerBoi;
-  console.log('The entity thinks itself funny. It believes it knows humor. The Eldritch Truth will teach it of humor.');
-}
 
-// Question/Answer #3
-var lifterBoi = prompt('Does Brandon even lift...brah?').toLowerCase();
-// If they say yes.
-if (lifterBoi === 'yes' || lifterBoi === 'y') {
-  alert('Indeed.');
-  document.getElementById('query3Answer').innerHTML = lifterBoi;
-  console.log('The entity responded to the third query in the affirmative. Correct.');
-} else if (lifterBoi === 'no' || lifterBoi === 'n') {
-  // If they say no.
-  alert('Are you a fool?');
-  document.getElementById('query3Answer').innerHTML = lifterBoi;
-  console.log('The entity responded to the third query in the negative. Incorrect.');
-} else {
-  // If they say anything else.
-  alert('You test the patience of powers cosmic...');
-  document.getElementById('query3Answer').innerHTML = lifterBoi;
-  console.log('The entity thinks itself funny. It believes it knows humor. The Eldritch Truth will teach it of humor.');
-}
 
-// Question/Answer #4
-var foodieBoi = prompt('Does Brandon ingest of the hated substance of the pit...LICORICE!?').toLowerCase();
-// If they say yes.
-if (foodieBoi === 'yes' || foodieBoi === 'y') {
-  alert('You are not a particularly sharp entity are you?');
-  document.getElementById('query4Answer').innerHTML = foodieBoi;
-  console.log('The entity responded to the fourth query in the affirmative. Incorrect.');
-} else if (foodieBoi === 'no' || foodieBoi === 'n') {
-  // If they say no.
-  alert('This is in alignment...');
-  document.getElementById('query4Answer').innerHTML = foodieBoi;
-  console.log('The entity responded to the fourth query in the negative. Correct.');
-} else {
-  // If they say anything else.
-  alert('I will sacrifice you to the ravening pit for your impudence.');
-  document.getElementById('query4Answer').innerHTML = foodieBoi;
-  console.log('The entity thinks itself funny. It believes it knows humor. The Eldritch Truth will teach it of humor.');
-}
 
-// Question/Answer #5
-var rebelBoi = prompt('Is Brandon preoccupied by the desires of the masses?').toLowerCase();
-// If they say yes.
-if (rebelBoi === 'yes' || rebelBoi === 'y') {
-  alert('To be so wrong...how can such a thing even draw breath? Much less exist?');
-  document.getElementById('query5Answer').innerHTML = rebelBoi;
-  console.log('The entity responded to the fifth query in the affirmative. Incorrect.');
-} else if (rebelBoi === 'no' || rebelBoi === 'n') {
-  // If they say no.
-  alert('You speak truly.');
-  document.getElementById('query5Answer').innerHTML = rebelBoi;
-  console.log('The entity responded to the fifth query in the negative. Correct.');
-} else {
-  // If they say anything else.
-  alert('Your soul will agony long after the stars have all grown cold.');
-  document.getElementById('query5Answer').innerHTML = rebelBoi;
-  console.log('The entity thinks itself funny. It believes it knows humor. The Eldritch Truth will teach it of humor.');
-}
+
+
+
+
+
+
 
