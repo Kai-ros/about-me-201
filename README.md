fdfd348ff7851f04b7b86498a94d8fd339f8e77d
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-07 07:43:43 -0700

    Final fixes on bugs that Nhu and myself encountered. Also hooked the UI back up.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # about-me-201
 A website about myself
+
+The code present on this page was worked on in tandem by 
+Brandon Hurrington and Nhu Trinh on June 6th, 2019. Brandon was the navigator and Nhu was the driver.
--- js/app.js
+++ js/app.js
@@ -1,20 +1,7 @@
 'use strict';
 
-/*
-  1. Is Brandon a gamer?
-  Answer Yes
-  2. Can Brandon sing?
-  Answer No
-  3. Does Brandon lift weights?
-  Answer Yes
-  4. Does Brandon eat licorice?
-  Answer No
-  5. Is Brandon concerned with popularity?
-  Answer No
-*/ 
 
 var correctAnswersTally = 0;
-var answer = false;
 var correctAnswers = ['y', 'n', 'y', 'n', 'n'];
 var promptReplies;
 var userAnswers = [];
@@ -36,22 +23,13 @@ var correctAlertMessages = [
 ];
 
 var incorrectAlertMessages = [
-  'Hrrmm...that is not accurate.',
+  'This is not the arena for frivolity...',
   'Are you certain that you know of the being, Brandon...?',
-  'Are you a fool?',
-  'You are not a particularly sharp entity are you?',
+  'Your path is one of folly and error.',
+  'I will sacrifice you to the ravening pit for your impudence.',
   'To be so wrong...how can such a thing even draw breath? Much less exist?'
 ];
 
-// var nonsenseAlertMessages = [
-//   'This is not the arena for frivolity...',
-//   'Your path is one of folly and error.',
-//   'You test the patience of powers cosmic...',
-//   'I will sacrifice you to the ravening pit for your impudence.',
-//   'Your soul will agony long after the stars have all grown cold.'
-// ];
-
-// Console log messages
 var correctConsoleLogMessages = [
   'The entity responded to the first query in the affirmative. Correct.',
   'The entity responded to the second query in the negative. Correct.',
@@ -68,7 +46,6 @@ var incorrectConsoleLogMessages = [
   'The entity responded to the fifth query in the affirmative. Incorrect.'
 ];
 
-var nonsenseConsoleMessage = 'The entity thinks itself funny. It believes it knows humor. The Eldritch Truth will teach it of humor.';
 
 var htmlTargets = [
   'query1Answer',
@@ -78,26 +55,25 @@ var htmlTargets = [
   'query5Answer'
 ];
 
-var username;
+var userName;
 var greetings = function(){
-  username = prompt('Tell us your name.');
+  userName = prompt('Tell us your name.');
 };
 
 greetings();
 
-
-alert(`Well met, ${username}. I will now pose you with queries about the being known as Brandon, as is my grand purview. You will respond in either an affirmative or a negative, in the form of a "yes" or a "no", "y" and "n" may also be substituted for either of these choices. Your ultimate worth on the cosmic stage shall be weighed, measured, and decided...`);
-
-
 var initialFiveQuestions = function(){
+
+  alert(`Well met, ${userName}. I will now pose you with queries about the being known as Brandon, as is my grand purview. You will respond in either an affirmative or a negative, in the form of a "yes" or a "no", "y" and "n" may also be substituted for either of these choices. Your ultimate worth on the cosmic stage shall be weighed, measured, and decided...`);
+
   for (var i = 0; i < promptQueries.length; i++) {
+
     promptReplies = prompt(promptQueries[i]).toLowerCase();
     userAnswers.push(promptReplies);
-    // document.getElementById(htmlTargets[i]).innerHTML = userAnswers[i];
-    // Check replies to the queries
+    document.getElementById(htmlTargets[i]).innerHTML = userAnswers[i];
     console.log(userAnswers);
-    if (userAnswers[i] === correctAnswers[i]) {
 
+    if (userAnswers[i] === correctAnswers[i]) {
       // If they answer correctly
       alert(correctAlertMessages[i]);
       console.log(correctConsoleLogMessages[i]);
@@ -107,58 +83,68 @@ var initialFiveQuestions = function(){
     // If they answer incorrectly.
     alert(incorrectAlertMessages[i]);
     console.log(incorrectConsoleLogMessages[i]);
+
   }
-}
+};
 
 initialFiveQuestions();
-
 
 var userGuess;
 
 var dreadedQuestionSix = function(){
+
+  alert(`Now we shall play a game of a different sort, ${userName}. I will ask you a question to suppose a number, you will have 4 tries to guess the answer correctly. `);
+
   for(var numGuesses = 0; numGuesses < 4; numGuesses++)
   {
     userGuess = prompt('What number is the sign of the beast?');
-    console.log(userGuess);
-    console.log(666);
-    if(userGuess === 666)
-    {
-
+    var scrubbedUserGuess = parseInt(userGuess);
+    if(scrubbedUserGuess === 666) {
       alert('Great guess!');
       correctAnswersTally++;
       break;
-
     } 
-    else if(userGuess > 666)
-    {
+    else if(scrubbedUserGuess > 666) {
       alert('Too high! Guess again.');
       continue;
     } 
-    else if(userGuess < 666)
-    {
+    else if(scrubbedUserGuess < 666) {
       alert('Too low! Guess again.');
       continue;
+    } 
+    else {
+      alert('That answer won\'t do at all.');
+      continue;   
     }
   }
-}
+
+};
 
 dreadedQuestionSix();
 
 
-// var infamousQuestionSeven = function(){
-//   var elderGods = ['Cthulu', 'Oryx', 'Yog-Sothoth', 'Azathoth', 'The Blackness from the Stars'];
-//   var godResponse = prompt('Tell me the name of an Elder God.');
+var infamousQuestionSeven = function(){
+  var elderGods = ['Cthulu', 'Oryx', 'Yog-Sothoth', 'Azathoth', 'The Blackness from the Stars'];
 
-//   for(userGuess = 0; userGuess < 6; userGuess++){
-//     for(var i = 0; i < 5; i++){
-//       if(userGuess === elderGods[i]){
-//         alert('great response.');
-//         break;
-//       }
-//     }
-//     alert('WRONG, try again.');
-//   }
-// }
+  loop1:
+  for(var i = 0; i < 6; i++){
 
-// infamousQuestionSeven();
+    var namedGods = prompt(`The great game advances ${userName}. Now you will tell me the name of an Elder God.`);
+
+    loop2:
+    for(var j = 0; j < 5; j++){
+
+      if(namedGods === elderGods[j]){
+        correctAnswersTally++;
+        alert(`Excellent, ${userName}. You have gotten ` + correctAnswersTally + ' answers correct. Your fate remains undecided.');
+        break loop1;
+      }
+
+    }
+    alert('Unacceptable...');
+
+  }
+};
+
+infamousQuestionSeven();
 
